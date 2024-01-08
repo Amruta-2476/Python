ce40cc69592923ca9b7c245de882ae9489043f8d
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-01-08 23:14:29 +0530

    first commit

---
+++ 01_Practice set/poem.py
@@ -0,0 +1,27 @@
+# 1)	WAP to print a poem in python
+
+# When you want to print a multi-line string then do similar to multi-line comment i.e. using ‘’’kdjkjkdksd’’’
+print('''Twinkle, twinkle, little star
+
+How I wonder what you are
+
+Up above the world so high
+
+Like a diamond in the sky
+
+Twinkle, twinkle little star
+
+How I wonder what you are
+
+
+When the blazing sun is gone
+
+When he nothing shines upon
+
+Then you show your little light
+
+Twinkle, twinkle, all the night
+
+Twinkle, twinkle, little star
+
+How I wonder what you are''')
---
+++ 01_hello.py
@@ -0,0 +1,13 @@
+import os #this is in built module
+# import flask  #this we need to install externally
+# ModuleNotFoundError: No module named 'flask'
+print("hello world")  
+# python doesn't have semicolon at the end
+
+# pip = package manager of python. with pip u can install a module in your system
+
+
+'''this is a
+multi-line comment
+written using triple single 
+quotes'''
