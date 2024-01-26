ad1c8bb61fcba7b6f5b60fe4a4e5587c625be3f6
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-01-26 18:55:25 +0530

    string

--- 03_Strings(str)/03_formatString.py
+++ 03_Strings(str)/03_formatString.py
@@ -11,6 +11,17 @@ itemNo = 567
 price = 49.95
 myorder = "I want {} pieces of item {} for {} dollars."
 print(myorder.format(quantity, itemNo, price))
+
 # OR
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemNo, price))
+
+# OR JUST MAKE SURE TO REARRANGE THEM IN THE .format() method
+myorder = "I want to pay {} dollars for {} pieces of item {}."
+print(myorder.format(price, quantity, itemNo)) 
+
+x = 5
+y = 10
+print(f"The sum of {x} and {y} is {x + y}")
+
+
