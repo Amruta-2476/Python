e175c4fcf3a54d97d9fbcf2c0c2d116f7465d8e6
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-05-28 19:20:39 +0530

    new change

--- PP_ESE_QB sem 4/17.py
+++ PP_ESE_QB sem 4/17.py
@@ -1,11 +1,14 @@
 try:
     result = int(input("Enter a number: ")) / 0
+    print("Result:", result)
+    list = [1,2,3,4,5]
+    print(list[10])
 except ValueError:
     # ValueError = if the input cannot be converted to an integer
     print("Error: Invalid input. Please enter a valid number.")
 
-# except ZeroDivisionError:
-#     print("Error: Division by zero")
+except ZeroDivisionError:
+    print("Error: Division by zero")
 
 except Exception as e:
     # generic exception handling
