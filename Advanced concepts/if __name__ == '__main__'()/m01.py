a3baabb944b90e07108084823877792d56d9573b
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-06-07 08:54:38 +0530

    if __name__ == '__main__'()

---
+++ Advanced concepts/if __name__ == '__main__'()/m01.py
@@ -0,0 +1,16 @@
+def greet(name): 
+    print("Hello, " + name)
+
+'''n = input("Enter your name: ")
+greet(n)'''
+# Output: 
+# Enter your name: John
+# Hello, John
+
+# lets say we want to use this function in another file/module m02.py 
+
+print(__name__)     # Output: __main__
+if __name__ == '__main__':
+    n = input("Enter your name: ")
+    greet(n)
+    print('this is m01.py')
---
+++ Advanced concepts/if __name__ == '__main__'()/m02.py
@@ -0,0 +1,20 @@
+import m01
+m01.greet('Ria')
+
+# Output: 
+# Enter your name: 
+# why do we get this output?
+# because the entire code in m01.py is executed when we import it in m02.py
+# so the input() function is executed and we get the output "Enter your name: "
+
+# to avoid this we use the following code in m01.py
+# if __name__ == '__main__':
+#     n = input("Enter your name: ")
+#     greet(n)
+# this way the code inside the if block will only be executed when we run m01.py and not when we import it in m02.py
+# so we will not get the output "Enter your name: " when we import m01.py in m02.py
+
+
+'''  Here __name__ == m01
+Therefore output is only
+Hello, Ria '''
