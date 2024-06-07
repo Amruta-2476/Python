9166610c52db9e1ada06dc8bdd43a187637eaade
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-06-07 13:48:36 +0530

    format change later

--- Advanced concepts/global/global.py
+++ Advanced concepts/global/global.py
@@ -1,1 +1,7 @@
 # https://www.youtube.com/watch?v=61a7UkDO50s&t=0s
+
+a = "Hello this is {} and age is {}".format("Amruta", 20)
+print(a)
+
+
+# print("Hello this is {} and age is {}".format("Amruta", 20))
