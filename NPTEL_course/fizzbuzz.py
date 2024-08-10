3f3523f32952ddc3470278b73788db63e56bfe8d
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-08-10 22:21:48 +0530

    fizzbuzz game

--- NPTEL_course/fizzbuzz.py
+++ NPTEL_course/fizzbuzz.py
@@ -2,10 +2,18 @@
 
 for i in range(1, 21):
     # print(i, end = ' ')
-    if (i % 3 == 0 and i % 5 == 0):
-        print(f'{i} = FizzBuzz')
-    elif (i % 3 == 0):
+    # if (i % 3 == 0 and i % 5 == 0):
+    #     print(f'{i} = FizzBuzz')
+    # elif (i % 3 == 0):
+    #     print(f'{i} = Fizz')
+    # elif (i % 5 == 0):
+    #     print(f'{i} = Buzz')
+    # else:
+    #     print(i)
+    if (i % 3 == 0):
         print(f'{i} = Fizz')
+        if ( i % 5 == 0):
+            print(f'{i} = FizzBuzz')
     elif (i % 5 == 0):
         print(f'{i} = Buzz')
     else:
