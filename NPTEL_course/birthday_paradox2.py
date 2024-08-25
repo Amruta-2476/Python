46e6bc3afb6b9c05a4b1e0231620e306ad74893b
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-08-25 11:26:47 +0530

    random birthday generator and duplicate bdays

--- NPTEL_course/birthday_paradox2.py
+++ NPTEL_course/birthday_paradox2.py
@@ -14,14 +14,12 @@ i = 0
 
 while i < 50:
     year = random.randint(1895, 2017)
-    
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         leap = 1
     else:
         leap = 0
         
     month = random.randint(1, 12)
-    
     if month == 2 and leap == 1:
         day = random.randint(1, 29)
     elif month == 2 and leap == 0:
